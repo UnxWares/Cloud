0a596d6bc7ca0aba13872029b06753c88f642ca9
Baptiste Gosselin <baptiste.gosselin@etu.unicaen.fr>
2025-08-04 11:59:31 +0200

    Added: Autoprefixer + PostCSS + SCSS

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,24 @@
-import { defineConfig } from "vite";
-import laravel from "laravel-vite-plugin";
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
 import { svelte } from "@sveltejs/vite-plugin-svelte";
+import autoprefixer from 'autoprefixer';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ["resources/css/app.css", "resources/js/app.js"],
+            input: ['resources/css/app.css', "resources/styles/app.scss", 'resources/js/app.js'],
             refresh: true,
+	    postcss: [
+            	autoprefixer()
+            ],
         }),
-        svelte({}),
+	svelte({
+		prebundleSvelteLibraries: true,
+	}),
     ],
+    css: {
+        	preprocessorOptions: {
+        	scss: {},
+    	},
+    },
 });
