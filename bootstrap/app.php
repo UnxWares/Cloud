7ef9f7395455e69a426f0e6cf3a4819f283131a1
Davidutz_ <71325082+DavidutzDev@users.noreply.github.com>
2026-02-19 02:00:35 +0100

    Enable TrustProxies middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use App\Http\Middleware\HandleInertiaRequests;
+use App\Http\Middleware\TrustProxies;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -17,6 +18,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->web(append: [
             HandleInertiaRequests::class,
+            TrustProxies::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
